250901f332d0489f57ba410da23c997cf01a40f1
bhavnaPatel-6 <pbhavna0987@gmail.com>
2026-06-21 22:26:37 +0530

    Fix frontend API and script links

--- checkout.js
+++ checkout.js
@@ -1,5 +1,4 @@
-const API_URL =
-"http://localhost:8080";
+
 const token = localStorage.getItem("token");
 
 
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 
-const API_URL = "http://localhost:8080";
 
 const token = localStorage.getItem("token");
 const role = localStorage.getItem("role");
